de8eeec162ba4a51b673e875290e60a50046a387
yasirmabbas-boop <yasir.m.abbas@gmail.com>
2026-02-21 18:48:00 +0300

    Fix double padding in top bar navbar

--- android/app/src/main/java/iq/ebay3/app/MainActivity.java
+++ android/app/src/main/java/iq/ebay3/app/MainActivity.java
@@ -1,42 +1,5 @@
 package iq.ebay3.app;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
-import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Disable edge-to-edge so navigation bar doesn't overlay content
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
-        
-        // Make sure the navigation bar doesn't cover our content
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-
-        // Create notification channel for Android 8.0+
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(
-                "default",
-                "Default Notifications",
-                NotificationManager.IMPORTANCE_HIGH
-            );
-            channel.setDescription("Default notification channel for push notifications");
-            channel.enableVibration(true);
-            channel.setShowBadge(true);
-
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            if (notificationManager != null) {
-                notificationManager.createNotificationChannel(channel);
-            }
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
